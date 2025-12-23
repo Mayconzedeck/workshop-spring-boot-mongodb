5242e637756922dfa1d85dce30e520ed1db96851
maycon zedeck <maykonlemos2@gmail.com>
2025-12-23 14:43:26 -0300

    Retrieve user by id

--- src/main/java/com/zdkproject/workshopmongo/resources/UserResource.java
+++ src/main/java/com/zdkproject/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +15,23 @@ import com.zdkproject.workshopmongo.dto.UserDTO;
 import com.zdkproject.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value= "/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> findall(){
+	public ResponseEntity<List<UserDTO>> findall() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
 
-}	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+}
